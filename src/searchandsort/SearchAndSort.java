6f9d2e32419e82779287d9c65873c3a7e6017b2d
marcusquirk <marcus@192.168.0.19>
2020-03-27 12:13:10 -0500

    Added insertion sort; revamped the main method

--- src/searchandsort/SearchAndSort.java
+++ src/searchandsort/SearchAndSort.java
@@ -1,9 +1,9 @@
-
 package searchandsort;
 
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -107,22 +107,136 @@ public class SearchAndSort {
 
     // TO-DO: Define a method that sorts a list
     // of integers using the insertion sort algorithm.
+    public static ArrayList<Integer> insertionSort(
+            ArrayList<Integer> unsortedList) {
+        ArrayList<Integer> sortedList = unsortedList;
+        for (int i = 0; i < sortedList.size() - 1; i++) {
+            boolean continueChecking = true;
+            int currentIndex = i;
+            while (continueChecking) {
+                continueChecking = Swap(sortedList, currentIndex);
+                if (currentIndex > 0) {
+                    currentIndex--;
+                }//if
+                else {
+                    continueChecking = false;
+                }//else
+            }//while
+        }//if
+        return unsortedList;
+    }
+
+    public static boolean Swap(ArrayList<Integer> someList, int firstIndex) {
+        if (someList.get(firstIndex) > someList.get(firstIndex + 1)) {
+            int helper = someList.get(firstIndex);
+            someList.set(firstIndex, firstIndex + 1);
+            someList.set(firstIndex + 1, helper);
+            return true;
+        }//If
+        else {
+            return false;
+        }//Else
+
+    }//Swap
+
     // TO-DO: Define a method that sorts a list
     // of integers using the merge sort algorithm.
+    
+    
+    /**
+     * The main method allows the user to test the different searching and
+     * sorting algorithms. It generates a random ArrayList of 15 elements, each
+     * a random integer from 1-19. It then asks the user what kind of method
+     * they would like to test. If the input is not recognised, the user is
+     * recommended to input 'h' to receive help about what inputs are valid.
+     * The valid inputs allow the user to test insertion sort, selection sort,
+     * merge sort, linear search, and binary search.
+     * @param args 
+     */
     public static void main(String[] args) {
         System.out.println("Searching and sorting algorithms");
+        System.out.println("A random ArrayList has been generated:");
         ArrayList<Integer> someList = new ArrayList<>();
         for (int i = 0; i < 15; i++) {
             someList.add(RNG.nextInt(20));
             System.out.print(someList.get(i) + " ");
         }
-        ArrayList<Integer> sortedList = selectionSort(someList);
-        System.out.println();
-        for (int i=0; i < sortedList.size(); i++){
-            System.out.print(sortedList.get(i)+" ");
-        }//for
-        
-        // TO-DO: Add code that tests the searching and sorting
-        // methods.
+
+        Scanner input = new Scanner(System.in); //create the scanner object called input
+
+        boolean keepLooping = true; //keep looping so the user doesn't need 
+        //to restart the program to try again
+        while (keepLooping) {
+            System.out.println("");
+            System.out.println("What method would you like to test?");
+            //ask for input
+            String action = input.nextLine(); //wait for user to input an string
+            switch (action.toLowerCase()) {
+                case "h":
+                case "help":
+                    System.out.println(
+                            "Type 'I' or 'Insertion' to perform an insertion sort \n"
+                            + "Type 'S' or 'Selection' to perform a selection sort \n"
+                            + "Type 'M' or 'Merge' to perform a merge sort \n"
+                            + "Type 'F', 'Find', or 'Search' to perform a linear search \n"
+                            + "Type 'B' or 'Binary' to perform a binary search "
+                            + "(the ArrayList will be sorted for you)\n"
+                            + "+Type 'Bye' to stop the program");
+                    break;
+                case "f":
+                case "find":
+                case "search":
+                case "linear search":
+                    System.out.print("Choose a number: "); //ask for input
+                    int num = input.nextInt(); //wait for user to input an double
+                    System.out.println("The index of " + num + " in the ArrayList is "
+                            + getIndex(someList, num));
+                    break;
+                case "insertion":
+                case "i":
+                case "insertion sort":
+                    ArrayList<Integer> sortedList = insertionSort(someList);
+                    System.out.println("Insertion:");
+                    for (int i = 0; i < sortedList.size(); i++) {
+                        System.out.print(sortedList.get(i) + " ");
+                    }//for
+                    System.out.println();
+                    break;
+                case "s":
+                case "selection":
+                case "selection sort":
+                    sortedList = selectionSort(someList);
+                    System.out.println("Selection:");
+                    for (int i = 0; i < sortedList.size(); i++) {
+                        System.out.print(sortedList.get(i) + " ");
+                    }//for
+                    break;
+                case "b":
+                case "binary":
+                case "binary search":
+                case "binary find":
+                    sortedList = selectionSort(someList);
+                    System.out.println("Sorted list:");
+                    for (int i = 0; i < sortedList.size(); i++) {
+                        System.out.print(sortedList.get(i) + " ");
+                    }//for
+                    System.out.println();
+                    System.out.println("Choose a number: "); //ask for input
+                    num = input.nextInt(); //wait for user to input an integer
+                    System.out.println("The index of " + num + " in the sorted ArrayList is "
+                            + getIndexBinary(someList, num, 0, someList.size()));
+                    break;
+                case "no":
+                case "bye":
+                case "goodbye":
+                case "good-bye":
+                    keepLooping = false;
+                    break;
+                default:
+                    System.out.println("Invalid input! Type 'h' for help.");
+                    break;
+            }//switch
+        } // while
     } // main( String [] )
+
 } // SearchAndSort
